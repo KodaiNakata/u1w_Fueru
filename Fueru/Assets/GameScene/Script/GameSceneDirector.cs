03ce15d171f16f853fdc3bf9a6d048ec9d3c6907
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2020-08-10 00:51:10 +0900

    コードをプッシュできるかのテスト(#1)

---
+++ Fueru/Assets/GameScene/Script/GameSceneDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * @file GameSceneDirector.cs
+ * @brief ゲーム画面の監督スクリプトのファイル
+ * @author Kodai Nakata
+ */
+/**
+ * @class GameSceneDirector
+ * @brief ゲーム画面の監督スクリプト用のクラス
+ */
+public class GameSceneDirector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
